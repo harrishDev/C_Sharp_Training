62e96e00decff23d9baf5b02027b627a12bf5ec3
Harrish <harrishoffical@gmail.com>
2025-03-24 00:25:48 +0530

    ass4 t2 completed

--- sql/assignment4.sql
+++ sql/assignment4.sql
@@ -1,6 +1,12 @@
 USE CMS
 
-drop table users
+select * from Users
+select * from Courier
+select * from CourierServices
+select * from Employee
+select * from Location
+select * from Payment
+
 create table Users(
 UserID INT PRIMARY KEY,  
 Name VARCHAR(255),  
@@ -52,3 +58,67 @@ PaymentDate DATE,
 FOREIGN KEY (CourierID) REFERENCES Courier(CourierID),  
 FOREIGN KEY (LocationID) REFERENCES Location(LocationID)
 )
+
+--task2
+--1.  List all customers
+select * from Users
+
+--2.  List all orders for a specific customer
+select * from Courier
+where SenderName='rohan'
+
+--3.  List all couriers:  
+select * from Courier
+
+--4.List all packages for a specific order:
+SELECT * FROM Courier
+WHERE CourierID = 6
+
+--5. List all deliveries for a specific courier
+select * from Courier
+where CourierID=3
+and Status='delivered'
+
+--6. List all undelivered packages: 
+select * from Courier
+where Status != 'delivered'
+
+--7. List all packages that are scheduled for delivery today:  
+update Courier
+set DeliveryDate='2025-03-24'
+where CourierID=6
+
+select * from Courier
+where DeliveryDate = CAST(getdate() as date) 
+
+--8.List all packages with a specific status:  
+select * from Courier
+where Status='in transit'
+
+--9.Calculate the total number of packages for each courier.  
+SELECT CourierID, COUNT(*) AS TotalPackages
+FROM Courier
+GROUP BY CourierID;
+
+--10.Find the average delivery time for each courier
+
+
+--11. List all packages with a specific weight range:  
+SELECT * FROM Courier
+WHERE Weight BETWEEN 4 AND 6
+
+--12. Retrieve employees whose names contain 'John'  
+select * from Employee
+where Name like '%John%'
+
+--13. Retrieve all courier records with payments greater than $50.
+select c.* from Courier c
+join Payment p on c.CourierID=p.CourierID
+where Amount>50
+
+select * from Users
+select * from Courier
+select * from CourierServices
+select * from Employee
+select * from Location
+select * from Payment
