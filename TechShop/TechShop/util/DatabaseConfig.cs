ba3b22f1122e0bcf224a1bbb05c9958563d31e78
Harrish <harrishoffical@gmail.com>
2025-04-15 09:33:07 +0530

    ass1 - tech shop added

---
+++ TechShop/TechShop/util/DatabaseConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace TechShop.util
+{
+    public static class DatabaseConfig
+    {
+        // Update your connection string here
+        public static string ConnectionString => "Server=DESKTOP-HNVF699;Database=TechShop;Trusted_Connection=True;TrustServerCertificate=True;";
+    }
+}
